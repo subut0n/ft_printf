1c9934c288a4f379d5d58adbc3846ca886db3182
subut0n <a.dzikowski@outlook.fr>
2021-01-29 14:02:59 +0100

    Matthieu troll avec le reboot

--- srcs/print_formated.c
+++ srcs/print_formated.c
@@ -6,7 +6,7 @@
 /*   By: addzikow <addzikow@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 14:31:49 by addzikow          #+#    #+#             */
-/*   Updated: 2021/01/29 12:27:36 by addzikow         ###   ########lyon.fr   */
+/*   Updated: 2021/01/29 13:52:35 by addzikow         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ int	print_formated(const char *ret, size_t i, t_options *options, va_list args)
 		nbr_char = charf(options, args);
 	if (options->specifier == 's')
 		nbr_char = strf(options, args);
+	if (options->specifier == 'x')
+		nbr_char = hexaminf(ret, i, options, args);
 	return (nbr_char);
 }
